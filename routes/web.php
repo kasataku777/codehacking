d7c6b97db3ee75933781a42affacf9fe5989b2c8
Takumi Kasai <takumi.jihen@gmail.com>
2020-01-04 15:50:32 +0900

    able to create post with static category

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,21 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/admin',function(){
+Route::get('logout','Auth\LoginController@logout');
 
-    return view('admin.index');
+
+Route::group(['middleware'=>'admin'],function() {
+
+    Route::get('/admin',function(){
+
+        return view('admin.index');
+    });
+
+    Route::resource('admin/users', 'AdminUsersController', ['as' => 'admin']);
+
+    Route::resource('admin/posts', 'AdminPostsController', ['as' => 'admin']);
+
+
+
+
 });
-Route::resource('admin/users','AdminUsersController',['as'=>'admin']);
-
-
